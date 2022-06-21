c44c7c440218e661c837aa3b9b0a133528f573a3
Davide Molitierno <>
2022-06-21 17:16:09 +0200

    Aggiunto db per le connessioni

--- projects/edipogram/provisioning.sh
+++ projects/edipogram/provisioning.sh
@@ -6,4 +6,5 @@ docker run -d --hostname localhost --name asw-consul --publish 8500:8500 consul
 
 echo Starting db for enigmi in a Docker Container 
 
-docker run --name db-enigmi -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -p 5438:5432 -d postgres
+docker run --name db-enigmi -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=enigmidb -e POSTGRES_USER=postgres -p 5438:5432 -d postgres
+docker run --name db-connessioni -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=connessionidb -e POSTGRES_USER=postgres -p 5439:5432 -d postgres
--- projects/edipogram/stop-app.sh
+++ projects/edipogram/stop-app.sh
@@ -6,3 +6,5 @@ docker stop asw-consul
 docker rm asw-consul 
 docker stop db-enigmi
 docker rm db-enigmi
+docker stop db-connessioni
+docker rm db-connessioni
