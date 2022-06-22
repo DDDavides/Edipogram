fa6bfc07d6fe8426d0a88e996ac1953fae8b03cc
Davide Galletti <d.galletti99@gmail.com>
2022-06-22 20:51:39 +0200

    Aggiunta logica per il publisching e sistemata dipendenza per kafka
    ~ enigmi/build.gradle
    ~ connessioni/build.gradle
    ~ connessioni/domain/ConnessioniService

--- projects/edipogram/connessioni/src/main/java/asw/edipogram/connessioni/domain/ConnessioniService.java
+++ projects/edipogram/connessioni/src/main/java/asw/edipogram/connessioni/domain/ConnessioniService.java
@@ -1,5 +1,7 @@
 package asw.edipogram.connessioni.domain;
 
+import asw.edipogram.common.api.event.DomainEvent;
+import asw.edipogram.connessioni.api.event.ConnessioneCreatedEvent;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,16 @@ public class ConnessioniService {
 	@Autowired
 	private ConnessioniRepository connessioniRepository;
 
+	@Autowired
+	private ConnessioniMessagePublisher connessioniMessagePublicher;
+
  	public Connessione createConnessione(String utente, String tipo) {
-		Connessione connessione = new Connessione(utente, tipo); 
+		Connessione connessione = new Connessione(utente, tipo);
 		connessione = connessioniRepository.save(connessione);
+
+		DomainEvent event = new ConnessioneCreatedEvent(connessione.getId(), connessione.getUtente(), connessione.getTipo());
+		connessioniMessagePublicher.publish(event);
+
 		return connessione;
 	}
 
