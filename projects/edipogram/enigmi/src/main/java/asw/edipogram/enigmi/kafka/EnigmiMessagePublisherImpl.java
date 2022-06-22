3280a7d350032ae5a38e30ad8d744ce22c2fb2b9
Davide Molitierno <>
2022-06-22 16:20:33 +0200

    Aggiunta interfaccia per gli eventi
    Aggiunto publisher per gli eventi creati

--- projects/edipogram/enigmi/src/main/java/asw/edipogram/enigmi/kafka/EnigmiMessagePublisherImpl.java
+++ projects/edipogram/enigmi/src/main/java/asw/edipogram/enigmi/kafka/EnigmiMessagePublisherImpl.java
@@ -5,6 +5,7 @@ import asw.edipogram.enigmi.domain.EnigmiMessagePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Component
 public class EnigmiMessagePublisherImpl implements EnigmiMessagePublisher{
@@ -12,10 +13,12 @@ public class EnigmiMessagePublisherImpl implements EnigmiMessagePublisher{
 	@Value("${asw.edipogram.enigmi.channel.out}")
     private String channel;
 
+	@Autowired
+	private KafkaTemplate<String, String> template;
+
 	@Override
 	public void publish(String message) {
-		// TODO Auto-generated method stub
-		
+		template.send(channel, message);
 	}
 
 }
