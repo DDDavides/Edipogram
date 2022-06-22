f95f2b00491c70973cc47a77e57f0647640d801d
Davide Galletti <d.galletti99@gmail.com>
2022-06-22 19:14:36 +0200

    Aggiunto connessioni-api-event

--- projects/edipogram/connessioni/src/main/java/asw/edipogram/connessioni/domain/ConnessioniService.java
+++ projects/edipogram/connessioni/src/main/java/asw/edipogram/connessioni/domain/ConnessioniService.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import java.util.logging.Logger;
 
-import java.util.logging.Logger; 
 import java.util.*; 
 
 @Service
 public class ConnessioniService {
-
 	@Autowired
 	private ConnessioniRepository connessioniRepository;
 
