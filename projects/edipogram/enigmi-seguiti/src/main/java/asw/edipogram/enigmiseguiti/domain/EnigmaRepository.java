b3368d77137a721095fd2f6efbd49baf85f3dafd
davidegattini <davidegatto99@gmail.com>
2022-06-23 20:05:32 +0200

    Aggiunti messaggi di log

--- projects/edipogram/enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/EnigmaRepository.java
+++ projects/edipogram/enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/EnigmaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Collection;
 
 public interface EnigmaRepository extends CrudRepository<Enigma, Long> {
-    @Query(nativeQuery = true, value = "SELECT e FROM enigma e WHERE e.tipo IN ?;")
+    @Query(value = "SELECT e FROM enigma e WHERE e.tipo IN ?;")
     public Collection<Enigma> getEnigmiByTipi(Collection<String> tipi);
 }
--- projects/edipogram/enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/EnigmiSeguitiService.java
+++ projects/edipogram/enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/EnigmiSeguitiService.java
@@ -1,14 +1,17 @@
 package asw.edipogram.enigmiseguiti.domain;
 
+import asw.edipogram.enigmiseguiti.rest.EnigmiSeguitiController;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.*; 
-import java.util.stream.*; 
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.*;
 
 @Service 
 public class EnigmiSeguitiService {
 
+	private final Logger logger = Logger.getLogger(EnigmiSeguitiService.class.toString());
 	@Autowired
 	private ConnessioneRepository connessioneRepository;
 
@@ -17,27 +20,33 @@ public class EnigmiSeguitiService {
 
 	/* Trova gli enigmi (in formato breve) degli utenti seguiti da utente. */ 
 	public Collection<Enigma> getEnigmiSeguiti(String utente) {
-		//TODO: deve accedere alla propria base di dati
-		Collection<Enigma> enigmiSeguiti = new TreeSet<>(); 
+		Collection<Enigma> enigmiSeguiti = new TreeSet<>();
+		logger.info("REPOSITORY method: getting connessioni by utente");
 		Collection<Connessione> connessioni = connessioneRepository.getConnessioniByUtente(utente);
 		Collection<String> tipiSeguiti = 
 			connessioni
 				.stream()
 				.map(c -> c.getTipo())
-				.collect(Collectors.toSet()); 
+				.collect(Collectors.toSet());
+		logger.info("REPOSITORY method return: \n" + tipiSeguiti);
 		if (tipiSeguiti.size()>0) {
+			logger.info("TipiSeguiti dall'utente > 0");
+			logger.info("REPOSITORY method: getting enigmi by tipi");
 			Collection<Enigma> enigmi = enigmaRepository.getEnigmiByTipi(tipiSeguiti);
+			logger.info("REPOSITORY method return: \n" + enigmi);
 			enigmiSeguiti.addAll(enigmi);
 		}
 		return enigmiSeguiti; 
 	}
 
 	//TODO: aggiungere logica applicativa per quando riceve EnigmaCreatedEvent
-	public Enigma addEnigma(Enigma enigma) { return enigmaRepository.save(enigma); }
+	public Enigma addEnigma(Enigma enigma) { 
+		return enigmaRepository.save(enigma); 
+	}
 
 	//TODO: aggiungere logica applicativa per ConnessioneCreatedEvent
-	public Connessione addConnessione(Connessione connessione) {
-		return connessioneRepository.save(connessione);
+	public Connessione addConnessione(Connessione connessione) { 
+		return connessioneRepository.save(connessione); 
 	}
 
 }
