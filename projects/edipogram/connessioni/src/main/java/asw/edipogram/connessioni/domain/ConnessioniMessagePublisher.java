23fb08cf0301a04f6ee78621edff9be1e44b0f5b
Davide Galletti <d.galletti99@gmail.com>
2022-06-24 17:18:49 +0200

    -

--- projects/edipogram/connessioni/src/main/java/asw/edipogram/connessioni/domain/ConnessioniMessagePublisher.java
+++
@@ -1,8 +0,0 @@
-package asw.edipogram.connessioni.domain;
-
-import asw.edipogram.common.api.event.DomainEvent;
-
-public interface ConnessioniMessagePublisher {
-    public void publish(DomainEvent message);
-
-}
